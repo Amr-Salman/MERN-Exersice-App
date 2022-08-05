62c25b11cfe79d80c55c7ac0fc8fbeb66be2f4ad
Amr-Salman <amrsalman27798@gmail.com>
2022-08-05 19:31:59 +0200

    Finishing the frontend

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
+const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
+
+  const [title, setTitle] = useState('');
+  const [load, setLoad] = useState('');
+  const [reps, setReps] = useState('');
+  const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const workout = { title, load, reps };
+
+    const response = await fetch('/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const json = await response.json();
+    if (!response.ok) {
+      setError(json.error);
+      setEmptyFields(json.emptyFields);
+    }
+    if (response.ok) {
+      setTitle('');
+      setLoad('');
+      setReps('');
+      setError(null);
+      setEmptyFields([]);
+      dispatch({ type: 'CREATE_WORKOUT', payload: json });
+    }
+  };
+  return (
+    <form className="create" onSubmit={submitHandler}>
+      <h3>Add a new workout</h3>
+      <label>Exercise title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
+      />
+
+      <label>Load (Kg):</label>
+      <input
+        type="number"
+        min={0}
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+        className={emptyFields.includes('load') ? 'error' : ''}
+      />
+
+      <label>Reps:</label>
+      <input
+        type="number"
+        min={0}
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+        className={emptyFields.includes('reps') ? 'error' : ''}
+      />
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default WorkoutForm;
